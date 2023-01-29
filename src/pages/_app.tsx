30c595f6327c36486b822104940ae86565eb05f3
Gabriel <gabriel.aconsurgens@gmail.com>
2023-01-29 01:51:44 -0300

    add header on _app as default component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header>
+        <div className="bg-gray-50 border-b border-gray-200 h-12 flex items-center px-5">
+          <div className="w-1/4"></div>
+          <div className="w-2/4"></div>
+          <div className="w-1/4 uppercase font-semibold text-gray-500">
+            <Link href="activity" className="mr-5">              
+                Atividade
+            </Link>
+            <Link href="listenlist" className="mr-5">
+              ListenList
+            </Link>
+            <Link href="profile" className="mr-5">
+              Perfil
+            </Link>
+          </div>
+        </div>
+      </header>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
